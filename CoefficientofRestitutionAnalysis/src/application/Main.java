953bba4fc5d753737f45102274daba1951ded0a1
ABHAY0PARIHAR <64008676+ABHAY0PARIHAR@users.noreply.github.com>
2021-11-25 18:17:49 +0530

    Added equation and minor bug fix

--- CoefficientofRestitutionAnalysis/src/application/Main.java
+++ CoefficientofRestitutionAnalysis/src/application/Main.java
@@ -14,8 +14,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
 		Scene scene = new Scene(root);
 		stage.setScene(scene);  
+		stage.setResizable(false);
 		stage.show();
-
     }
     
     public static void main(String[] args) {
