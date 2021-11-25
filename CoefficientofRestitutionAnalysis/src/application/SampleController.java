9e039347f1784629abe58ebe311294ff8936f1a0
ashutosh verma <ashutoshverma5555@gmail.com>
2021-11-25 15:37:48 +0530

    added ball class

--- CoefficientofRestitutionAnalysis/src/application/SampleController.java
+++ CoefficientofRestitutionAnalysis/src/application/SampleController.java
@@ -2,34 +2,19 @@ package application;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextArea;
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.animation.TranslateTransition;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Bounds;
-import javafx.geometry.Orientation;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.CullFace;
-import javafx.scene.shape.DrawMode;
-import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Translate;
 import javafx.util.Duration;
 
 public class SampleController implements Initializable {
@@ -53,10 +38,11 @@ public class SampleController implements Initializable {
 	private JFXTextArea heightLabel; // textArea to show height label
 	@FXML
 	private JFXTextArea scale;
+
 	private double height = 400, topBorder = 250; // height is a height of ball from bottom topBorder is a height of
 													// upper bound from top.
-	private double x = 650; // x-coordinate of ball
-	private double y = 200; // y- coordinate f ball
+
+//	private double y = 200; 
 	private double ce = 1; // coefficientofRestitution
 	private boolean resetAll = false; // to reset screen
 	private String printHeight = ""; // this string will get updated whenever ball is at the peak height.
@@ -66,28 +52,33 @@ public class SampleController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		scale.setText("Slider-1:\nHeight:0 to 400\nSlider-2:\nCoefficient of Restitution: 0 to 1\ne = "+  (double)Math.round(ce * 100) / 100);
+		Ball ball = new Ball();
+		ball.setX(650);// x-coordinate of ball
+		ball.setY(200);// y- coordinate f ball
+		scale.setText("Slider-1:\nHeight:0 to 400\nSlider-2:\nCoefficient of Restitution: 0 to 1\ne = "
+				+ (double) Math.round(ce * 100) / 100);
 		slider2.setValue(400); // set initial height
 		slider1.setValue(1.0); // set initial coefficient of restitution
 		height = slider2.getValue(); // get initial height
 		ce = slider1.getValue(); // get initial coefficient of restitution.
 		topBorder = 600 - height; // Upper bound of ball
-		y = topBorder; // y-coordinate of ball
-		sphere.relocate(x, y); // To relocate the sphere.
+		ball.setY(topBorder); // y-coordinate of ball
+		ball.ballPosition(sphere); // To relocate the sphere.
 		// To detect changes in height;
 		slider2.valueProperty().addListener(new ChangeListener<Number>() {
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				height = (double) newValue;
 				topBorder = 600 - height;
-				y = topBorder;
-				sphere.relocate(x, y);
+				ball.setY(topBorder);
+				ball.ballPosition(sphere);
 			}
 		});
 		// to detect changes in value of CoefficientoRestitution;
 		slider1.valueProperty().addListener(new ChangeListener<Number>() {
 			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
 				ce = (double) newValue;
-				scale.setText("Slider-1:\nHeight:0 to 400\nSlider-2:\nCoefficient of Restitution: 0 to 1\ne = "+  (double)Math.round(ce * 100) / 100);
+				scale.setText("Slider-1:\nHeight:0 to 400\nSlider-2:\nCoefficient of Restitution: 0 to 1\ne = "
+						+ (double) Math.round(ce * 100) / 100);
 			}
 		});
 		// To start the bouncing of ball;
@@ -96,39 +87,34 @@ public class SampleController implements Initializable {
 				resetAll = false;
 				printHeight = "";
 				label = 1;
+				printCount = 1;
 				textHeightLabel = "Height " + label + ": \n";
 				double tempHeight = (double) Math.round(height * 100) / 100;
 				printHeight += String.valueOf(tempHeight) + "\n";
 				showHeight.setText(String.valueOf(printHeight));
 				heightLabel.setText(String.valueOf(textHeightLabel));
+				slider1.setDisable(true);
+				slider2.setDisable(true);
 				Timeline timeline = new Timeline();
 				KeyFrame kf = new KeyFrame(Duration.seconds(0), new EventHandler<ActionEvent>() {
 					int speedY = 5;
-
+                       
 					@Override
 					public void handle(ActionEvent actionEvent) {
 						if (resetAll == true || printCount == 10) {
-
-							slider1.setDisable(false);
-							slider2.setDisable(false);
-							slider2.setValue(400);
-							slider1.setValue(1);
-							topBorder = 600 - height;
-							y = topBorder;
-							sphere.relocate(x, y);
-							printCount = 1;
-							label = 1;
+                            
+							setToInitial(ball);
 							timeline.stop();
 						}
-						y = y + speedY;
-                        
-						if (y >= 600 &&!resetAll) {
+						ball.setY(ball.getY() + speedY);
+
+						if (ball.getY() >= 600 && !resetAll) {
 							speedY = (-1) * speedY;
 							double temp = height;
 							height = height * ce * ce;
 							topBorder += temp - height;
 						}
-						if (y <= topBorder&&!resetAll) {
+						if (ball.getY() <= topBorder && !resetAll) {
 							speedY = (-1) * speedY;
 							label++;
 							double tempHeight = (double) Math.round(height * 100) / 100;
@@ -138,29 +124,20 @@ public class SampleController implements Initializable {
 							showHeight.setText(printHeight);
 							printCount++;
 						}
-						sphere.relocate(x, y);
-						
+						ball.ballPosition(sphere);
+
 					}
 				});
 				timeline.getKeyFrames().addAll(kf, new KeyFrame(Duration.millis(20)));
 				timeline.setCycleCount(Timeline.INDEFINITE);
 				timeline.play();
-				btn.setDisable(true); // Whenever we start our simulation start button get disabled. 
+				btn.setDisable(true); // Whenever we start our simulation start button get disabled.
 			}
 		};
-		btn.setOnAction(event); //Start button action
-         //Rest button function;
+		btn.setOnAction(event); // Start button action
+		// Rest button function;
 		EventHandler<ActionEvent> reset = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent r) {
-				slider1.setDisable(false);
-				slider2.setDisable(false);
-				slider2.setValue(400);
-				slider1.setValue(1);
-				topBorder = 600 - height;
-				y = topBorder;
-				sphere.relocate(x, y);
-				printCount = 1;
-				label = 1;
 				printHeight = "";
 				textHeightLabel = "";
 				heightLabel.setText(String.valueOf(textHeightLabel));
@@ -172,5 +149,17 @@ public class SampleController implements Initializable {
 		resetBtn.setOnAction(reset); // reset button action;
 
 	}
-
+    public void setToInitial(Ball ball) {
+    	this.slider1.setDisable(false);
+		this.slider2.setDisable(false);
+		this.slider2.setValue(400);
+		this.slider1.setValue(1);
+		this.ce = 1;
+	    this.height = 400;
+		this.topBorder = 600 - this.height;
+		ball.setY(topBorder);
+		ball.ballPosition(sphere);
+		printCount = 1;
+		label = 1;
+    }
 }
